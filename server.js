385732bbca6f8643f8911e2e57d2fc1f4c3b4265
Ali Shair <alishair7071@gmail.com>
2026-02-06 02:19:12 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express= require('express');
+const app= express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🚀 Express test server is running!");
+});
+
+
+const PORT= 8000;
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
