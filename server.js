ddca1cbb72aa14f8db4eb87351bc86151c1e3b9c
Ali Shair <alishair7071@gmail.com>
2026-02-07 14:05:36 +0500

    .

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app= express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("🚀 Express test server is running!");
+  res.send("Express test server is running!");
 });
 
 
